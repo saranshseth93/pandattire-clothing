7682016e98bdddbe98bf3b2a1e3ca0000b81b2da
Saransh <saranshseth93@gmail.com>
2020-04-02 22:09:33 +0530

    Removed service worker

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -19,6 +19,12 @@ const HomePage = () => (
             </div>
             <div className = 'menu-item'>
                 <div className = 'content'>
+                    <h1 className = 'title'>Sneakers</h1>
+                    <span className = 'subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className = 'menu-item'>
+                <div className = 'content'>
                     <h1 className = 'title'>Womens</h1>
                     <span className = 'subtitle'>SHOP NOW</span>
                 </div>
