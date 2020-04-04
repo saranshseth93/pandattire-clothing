74cfd2ba53a55b3c4e644bb7ea3a1c00540675a3
Saransh <saranshseth93@gmail.com>
2020-04-04 22:59:32 +0530

    Added header menu

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import './App.css';
+import Header from './components/header/header.component';
 
 import HomePage from './pages/homepage/homepage.component';
 
@@ -9,9 +10,10 @@ import HomePage from './pages/homepage/homepage.component';
 function App() {
   return (
     <div>
+    <Header />
     <Switch>
-    <Route exact path='/' component={HomePage} />
-    <Route exact path='/shop' component={ShopPage} />
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/shop' component={ShopPage} />
     </Switch>
     </div>
   );
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { ReactComponent as Logo } from '../../assets/pandattire.svg';
+import './header.style.scss';
+
+import './header.component';
+
+const Header = () => (
+<div className='header'>
+    <Link to='/' className='logo-container'>
+        <Logo className='logo' />
+    </Link>
+    <div className='options'>
+        <Link className='option' to='/shop'>
+            SHOP
+        </Link>
+        <Link className='option' to='/contact'>
+            CONTACT
+        </Link>
+    </div>
+</div>
+);
+
+export default Header;
